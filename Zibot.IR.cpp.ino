3e207a4b5a7171015bbe2d48db9e91e4d5739372
KenioBastos <31421599+KenioBastos@users.noreply.github.com>
2017-08-28 19:55:55 -0300

    Add files via upload

---
+++ Zibot.IR.cpp.ino
@@ -0,0 +1,44 @@
+/*
+  Zibot.IR.h - Biblioteca para pinagem e leitura de sensores infravermelhos.
+  Criada por Kênio Bastos, dia 28 de agosto de 2017.
+  Lançada para domínio público.
+*/
+
+#include "Arduino.h"
+#include "Zibot.IR.h"
+
+Infravermelho::Infravermelho(int AO, int DO, int VCC, int GND);
+{
+
+  pinMode(AO, INPUT);
+  pinMode(DO, INPUT);
+  pinMode(GND, OUTPUT);
+  pinMode(VCC, OUTPUT);
+
+  digitalWrite(GND, LOW);
+  digitalWrite(VCC, HIGH);
+
+  _AO = AO;
+  _DO = DO;
+  _GND = GND;
+  _VCC = VCC;
+
+}
+
+void Infravermelho::LeituraAnalogica()
+{
+  Serial.print("O seu sensor está lendo... ");
+  Serial.println(analogRead(_AO));
+  
+  MillisAnterior = millis();
+  do {} while (millis() - MillisAnterior <= 750);
+}
+
+void Infravermelho::LeituraDigital()
+{
+  Serial.print("O seu sensor está lendo... ");
+  Serial.println(digitalRead(_DO));
+  
+  MillisAnterior = millis();
+  do {} while (millis() - MillisAnterior <= 750);
+}
---
+++ Zibot.IR.h.ino
@@ -0,0 +1,29 @@
+/*
+  Zibot.IR.h - Biblioteca para pinagem e leitura de sensores infravermelhos.
+  Criada por Kênio Bastos, dia 28 de agosto de 2017.
+  Lançada para domínio público.
+*/
+
+#ifndef Zibot.IR_h
+
+#define Zibot.IR_h
+#include "Arduino.h"
+
+class Infravermelho
+{
+  public:
+    Infravermelho(int AO, int DO, int VCC, int GND);
+
+    void LeituraAnalogica();
+    void LeituraDigital();
+
+  private:
+    int _AO;
+    int _DO;
+    int _GND;
+    int _VCC;
+    
+    unsigned long MillisAnterior;
+};
+
+#endif
