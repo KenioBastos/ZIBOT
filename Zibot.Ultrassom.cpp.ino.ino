0a23d3d796b5eb09faa2e9fedda5014ed63d8ea3
KenioBastos <31421599+KenioBastos@users.noreply.github.com>
2017-09-23 14:46:31 -0300

    Update Zibot.Ultrassom.cpp.ino.ino

--- Zibot.Ultrassom.cpp.ino.ino
+++ Zibot.Ultrassom.cpp.ino.ino
@@ -1,6 +1,6 @@
 /*
   Zibot.Ultrassom.h - Biblioteca para pinagem e leitura de sensores ultrassônicos.
-  Criada por Kênio Bastos, dia 21 de setembro de 2017.
+  Criada por Kênio Bastos, dia 23 de setembro de 2017.
   Lançada para domínio público.
 */
 
@@ -32,20 +32,21 @@ Ultrassom::Ultrassom(int Trig, int Echo, int VCC.Ultra, int GND.Ultra);
 void Ultrassom::LeituraUltrassom()
 {
   digitalWrite(_Trig, LOW);
-
-  MicrosAnterior = micros();
+  unsigned long MicrosAnterior = micros();
   do {} while (micros() - MicrosAnterior <= 2);
 
   digitalWrite(_Trig, HIGH);
-
   MicrosAnterior = micros();
   do {} while (micros() - MicrosAnterior <= 10);
 
-  Duracao = pulseIn(_Echo, HIGH);
-  Distancia = Duracao * 0.034 / 2;
+  digitalWrite(_Trig, LOW);
 
-  Serial.print("O seu sensor ultrassonico esta lendo... ");
-  Serial.println(Distancia);
+  long Duracao = pulseIn(_Echo, HIGH);
+  float Distancia = Duracao * 0.034 / 2;
+
+  Serial.print("O seu sensor esta lendo... ");
+  Serial.print(Distancia);
+  Serial.println(" cm");
 
   MillisAnterior = millis();
   do {} while (millis() - MillisAnterior <= 750);
@@ -58,17 +59,37 @@ void Ultrassom::ChecagemUltrassom()
   Serial.println("Sera feita uma analise de qualidade do seu sensor.");
 
   MillisAnterior = millis();
-  do {} while (millis() - MillisAnterior <= 500);
+  do {} while (millis() - MillisAnterior <= 5000);
+
+  Serial.println();
 
   //--------------------------------------------------------------------------------------->  LEITURA DE TESTE;
 
   Serial.println("Posicione seu sensor a 10cm de algum objeto.") ;
-  Serial.println("Lembre-se que superfícies curvas afetam a leitura.");
-  Serial.println("A leitura de teste sera feita apos 5 segundos.");
-
+  MillisAnterior = millis();
+  do {} while (millis() - MillisAnterior <= 2000);
+  Serial.println("Lembre-se que a trajetória das ondas afeta a leitura.");
+  MillisAnterior = millis();
+  do {} while (millis() - MillisAnterior <= 2000);
+  Serial.println("A leitura de teste sera feita apos 10 segundos.");
   MillisAnterior = millis();
   do {} while (millis() - MillisAnterior <= 2000);
 
+  Serial.println("Dez...");
+  MillisAnterior = millis();
+  do {} while (millis() - MillisAnterior <= 1000);
+  Serial.println("Nove...");
+  MillisAnterior = millis();
+  do {} while (millis() - MillisAnterior <= 1000);
+  Serial.println("Oito...");
+  MillisAnterior = millis();
+  do {} while (millis() - MillisAnterior <= 1000);
+  Serial.println("Sete...");
+  MillisAnterior = millis();
+  do {} while (millis() - MillisAnterior <= 1000);
+  Serial.println("Seis...");
+  MillisAnterior = millis();
+  do {} while (millis() - MillisAnterior <= 1000);
   Serial.println("Cinco...");
   MillisAnterior = millis();
   do {} while (millis() - MillisAnterior <= 1000);
@@ -88,66 +109,75 @@ void Ultrassom::ChecagemUltrassom()
   //--------------------------------------------------------------------------------------->  CÁLCULO DA DISTÂNCIA;
 
   digitalWrite(_Trig, LOW);
-  MicrosAnterior = micros();
+  unsigned long MicrosAnterior = micros();
   do {} while (micros() - MicrosAnterior <= 2);
 
   digitalWrite(_Trig, HIGH);
   MicrosAnterior = micros();
   do {} while (micros() - MicrosAnterior <= 10);
+  
+  digitalWrite(_Trig, LOW);
 
-  Duracao = pulseIn(_Echo, HIGH);
-  Distancia = Duracao * 0.034 / 2;
+  long Duracao = pulseIn(_Echo, HIGH);
+  float Distancia = Duracao * 0.034 / 2;
 
-  DistanciaTeste = Distancia;
-
+  float DistanciaTeste = Distancia;
   Serial.println("Leitura de teste feita e armazenada!");
 
   MillisAnterior = millis();
-  do {} while (millis() - MillisAnterior <= 500);
+  do {} while (millis() - MillisAnterior <= 2000);
 
 
   //--------------------------------------------------------------------------------------->  RESULTADOS;
 
   Serial.println("Hora dos resultados...");
-
   MillisAnterior = millis();
-  do {} while (millis() - MillisAnterior <= 500);
+  do {} while (millis() - MillisAnterior <= 2000);
 
-  if (DistanciaTeste = 10)
+  if (DistanciaTeste >= 10 && DistanciaTeste < 11)
   {
     Serial.println("Seu sensor esta funcionando corretamente.");
+    MillisAnterior = millis();
+    do {} while (millis() - MillisAnterior <= 2000);
 
     Serial.print("O sensor, a uma distancia de 10cm, le...");
     Serial.println(DistanciaTeste);
+    MillisAnterior = millis();
+    do {} while (millis() - MillisAnterior <= 2000);
 
     Serial.println("Continue usando-o a vontade.");
-
     MillisAnterior = millis();
-    do {} while (millis() - MillisAnterior <= 1000);
+    do {} while (millis() - MillisAnterior <= 2000);
 
-  } else if (DistanciaTeste >= 8 && DistanciaTeste <= 12)
+  } else if (DistanciaTeste < 10 && DistanciaTeste >= 11 && DistanciaTeste >= 9 && DistanciaTeste < 12)
   {
     Serial.println("Seu sensor esta funcionando, porem de maneira nao ideal.");
+    MillisAnterior = millis();
+    do {} while (millis() - MillisAnterior <= 2000);
 
     Serial.print("O sensor, a uma distancia de 10cm, le...");
     Serial.println(DistanciaTeste);
-
-    Serial.println("Considere olhar nosso guia para solucao de problemas.");
-
     MillisAnterior = millis();
-    do {} while (millis() - MillisAnterior <= 1000);
+    do {} while (millis() - MillisAnterior <= 2000);
+
+    Serial.println("Considere ler nosso manual de instruções.");
+    MillisAnterior = millis();
+    do {} while (millis() - MillisAnterior <= 2000);
 
   } else
   {
     Serial.println("Seu sensor nao esta funcionando corretamente.");
+    MillisAnterior = millis();
+    do {} while (millis() - MillisAnterior <= 2000);
 
     Serial.print("O sensor, a uma distancia de 10cm, le...");
     Serial.println(DistanciaTeste);
+    MillisAnterior = millis();
+    do {} while (millis() - MillisAnterior <= 2000);
 
     Serial.println("Troque-o imediatamente.");
-
     MillisAnterior = millis();
-    do {} while (millis() - MillisAnterior <= 1000);
+    do {} while (millis() - MillisAnterior <= 2000);
+
   }
 }
-
