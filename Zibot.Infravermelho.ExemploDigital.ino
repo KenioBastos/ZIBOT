8221ba14b58b5e5aa7d73e11ae403a8d179536dd
KenioBastos <31421599+KenioBastos@users.noreply.github.com>
2017-09-23 12:18:23 -0300

    Update Zibot.Infravermelho.ExemploDigital.ino

--- Zibot.Infravermelho.ExemploDigital.ino
+++ Zibot.Infravermelho.ExemploDigital.ino
@@ -1,14 +1,16 @@
 /*
-  Zibot.IR.h - Biblioteca para pinagem, leitura e avaliação de sensores infravermelhos.
-  Criada por Kênio Bastos, dia 28 de agosto de 2017.
+  Zibot.Infravermelho.h - Biblioteca para pinagem, leitura e avaliação de sensores óticos infravermelhos.
+  Criada por Kênio Bastos, dia 23 de setembro de 2017.
   Lançada para domínio público.
 */
 
+// ATENÇÃO! PARA PLENO ENTENDIMENTO DO CÓDIGO, CONSIDERE LER NOSSO MANUAL DE INSTRUÇÕES!
+
 #include <Zibot.Infravermelho.h>
 
-// Declaração do sensor infravermelho, assim como pinagem. Porta de sinal: 9, alimentação positiva (VCC): 10 e alimentação negativa (GND): 11.
+// Declaração do sensor infravermelho, assim como pinagem.
 
-Infravermelho SensorIR_Digital(0, 9, 10, 11);
+Infravermelho SensorInfravermelho(0, 9, 10, 11);
 
 // Declaração da variável responsável pela escolha de qual dos dois comandos é executado.
 
@@ -16,28 +18,31 @@ char Comando;
 
 void setup()
 {
+  // Inicialização do monitor serial.
+  
   Serial.begin(9600);
 }
 
 void loop()
 {
-
   // 'Comando' recebe um sinal do monitor serial.
+
+  Serial.println ("Escolha qual comando quer executar. Checagem ou Leitura?");
+  Serial.println ("Pressione 'l' para Leitura, e 'c' para Checagem.");
 
   Comando = Serial.read();
 
-  /*
-    LeituraDigital  -> (1);
-    ChecagemDigital -> (2);
-    Comandos para (1) exibir os valores lidos pelo sensor e (2) para fazer uma avaliação da leitura.
-    Digitando "p" no monitor serial, o comando (1) para e o (2) começa.
-  */
+  // Leitura;
 
-  do {
-    SensorIR_Digital.LeituraDigital();
-  } while (Comando != 'p');
+  if (Comando = 'l')
+  {
+    SensorInfravermelho.LeituraDigital();
+  } 
 
-  SensorIR_Digital.ChecagemDigital();
-
+  // Checagem;
+  
+  else if (Comando = 'c')
+  {
+    SensorInfravermelho.ChecagemDigital();
+  }
 }
-
